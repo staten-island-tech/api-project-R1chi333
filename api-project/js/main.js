00d69932266494e666fba8254a1735162a5c2e5e
richie <113203265+R1chi333@users.noreply.github.com>
2023-01-10 14:41:50 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,1 +1,8 @@
 import "../style/style.css";
+const startscreenDoMS = {
+    carrotcastle: document.getElementById("carrotcastle")
+}
+startscreenDoMS.carrotcastle.addEventListener("click", function(){
+    startscreenDoMS.carrotcastle.classList.add("down")
+    startscreenDoMS.carrotcastle.classList.remove("up")
+})
