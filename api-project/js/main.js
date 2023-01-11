8cc84cfe07d2aa429a09a2668d35fbfc4806d0ff
richie <113203265+R1chi333@users.noreply.github.com>
2023-01-11 11:29:39 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,8 +1,11 @@
 import "../style/style.css";
 const startscreenDoMS = {
-    carrotcastle: document.getElementById("carrotcastle")
+    carrotcastle: document.getElementById("carrotcastle"),
+    heading: document.getElementById("heading"),
+    grass: document.getElementById("grass")
 }
 startscreenDoMS.carrotcastle.addEventListener("click", function(){
     startscreenDoMS.carrotcastle.classList.add("down")
     startscreenDoMS.carrotcastle.classList.remove("up")
+    startscreenDoMS.heading.remove();
 })
