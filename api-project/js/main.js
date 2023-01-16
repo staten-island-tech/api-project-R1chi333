786cf2b7836a63355d21011f67415365de4b7a93
richie <113203265+R1chi333@users.noreply.github.com>
2023-01-15 21:46:09 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,34 +1,15 @@
 import "../style/style.css";
-const startscreenDoMS = {
-    loadingin: document.getElementById("loadingin"),
-    carrotcastle: document.getElementById("carrotcastle"),
-    heading: document.getElementById("heading"),
-    grass: document.getElementById("grass"),
-    grass2: document.getElementById("grass2"),
-    bun1: document.getElementById("bun1"),
-    bun3: document.getElementById("bun3"),
-    skipbtncon: document.getElementById("skipbtncon"),
-    bunflag: document.getElementById("bunflag")
+const homepage = {
+    container: document.getElementById("con"),
+    ccc: document.getElementById("ccc"),
+    ccc2: document.getElementById("createyourcharacterbtn2")
 }
-startscreenDoMS.carrotcastle.addEventListener("click", function(){
-    setTimeout(() => {
-        startscreenDoMS.carrotcastle.classList.add("down")
-        startscreenDoMS.carrotcastle.classList.remove("up")
-        startscreenDoMS.grass.classList.add("grassup")
-        startscreenDoMS.grass.classList.remove("grassdown")
-        startscreenDoMS.grass2.classList.add("grass2up")
-        startscreenDoMS.grass2.classList.remove("grass2down")
-        setTimeout(() => {
-            startscreenDoMS.loadingin.remove();
-            document.body.style.backgroundColor = "rgb(255, 255, 255)";
-          }, 1550)
-      }, 1000)
-    startscreenDoMS.skipbtncon.classList.add("hide")
-    startscreenDoMS.bun1.classList.add("bungoup")
-    startscreenDoMS.bun1.classList.remove("bunbefore")
-    startscreenDoMS.bun3.classList.add("bungoup")
-    startscreenDoMS.bun3.classList.remove("bunbefore")
-    startscreenDoMS.bunflag.classList.add("flagraised")
-    startscreenDoMS.bunflag.classList.remove("flagbefore")
-    startscreenDoMS.heading.classList.add("hide")
+function cccp(){
+    homepage.container.innerHTML = ""
+}
+homepage.ccc.addEventListener("click", function(){
+    cccp();
+})
+homepage.ccc2.addEventListener("click", function(){
+    cccp();
 })
