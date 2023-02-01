8027d8a058cc38aed6069868a0249584525ef657
richie <113203265+R1chi333@users.noreply.github.com>
2023-01-31 19:00:45 -0500

    .

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,15 +1,24 @@
 import "../style/style.css";
+const url = "https://api.open5e.com/races/";
 const homepage = {
     container: document.getElementById("con"),
     ccc: document.getElementById("ccc"),
     ccc2: document.getElementById("createyourcharacterbtn2")
 }
-function cccp(){
-    homepage.container.innerHTML = ""
+async function getData(url){
+    try  {
+
+    } catch (error) {
+        console.log(error)
+    }
 }
-homepage.ccc.addEventListener("click", function(){
-    cccp();
-})
-homepage.ccc2.addEventListener("click", function(){
-    cccp();
-})
+getData(url);
+// function cccp(){
+//     homepage.container.innerHTML = ""
+// }
+// homepage.ccc.addEventListener("click", function(){
+//     cccp();
+// })
+// homepage.ccc2.addEventListener("click", function(){
+//     cccp();
+// })
